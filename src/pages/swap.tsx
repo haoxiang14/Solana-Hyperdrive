04bc9bc83cc37bf7513e06b9f0eb1210bc639964
Tan Hao Xiang <Tp060906@mail.apu.edu.my>
2023-09-27 13:46:19 +0800

    Update API

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -1,7 +1,26 @@
+import { request } from "http";
 import type { NextPage } from "next";
 import Head from "next/head";
 
 const Swap: NextPage = (props) => {
+  const geckoAPI = "https://api.geckoterminal.com/api/v2/networks/solana/pools" 
+  
+  fetch(geckoAPI)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.data[0].attributes.price_change_percentage.h24);
+      // const priceChangeH24 = parseFloat(data.attributes.price_change_percentage.h24)
+      // const sortedData = {gainers: [], losers: []}
+      // if (!isNaN(priceChangeH24)) {
+      //   if (priceChangeH24 > 0) {
+      //     sortedData.gainers.push(data);
+      //   } else if (priceChangeH24 < 0) {
+      //     sortedData.losers.push(data);
+      //   }
+      // }
+      
+      // console.log(JSON.stringify(sortedData, null, 2));
+    });
   return (
     <div>
       <Head>
